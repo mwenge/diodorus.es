dd813db8700ca40d1ea64791fcbbb4fb2bca51df
Robert Hogan <robhogan@gmail.com>
2019-04-10 20:59:30 +0100

    Update

---
+++ Andocides_On_his_Return/serviceworker.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var CACHE_NAME = 'Andocides_On_his_Return';
+var urlsToCache = [
+  '../book.css',
+  '../book.js',
+  '../atobAndBtoaTogether.min.js',
+  '../GFSDidot.css',
+  '../GFSDidot.ttf',
+  '../OpenSans.css',
+  '../OpenSans.ttf',
+  '../Cinzel.css',
+  '../Cinzel.ttf',
+  '../CormorantGaramond.css',
+  '../CormorantGaramond.ttf',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+urlsToCache.push("Andocides_On_his_Return_2.html");
+urlsToCache.push("translation.js");
---
+++ Demosthenes_Against_Pantaenetus/serviceworker.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var CACHE_NAME = 'Demosthenes_Against_Pantaenetus';
+var urlsToCache = [
+  '../book.css',
+  '../book.js',
+  '../atobAndBtoaTogether.min.js',
+  '../GFSDidot.css',
+  '../GFSDidot.ttf',
+  '../OpenSans.css',
+  '../OpenSans.ttf',
+  '../Cinzel.css',
+  '../Cinzel.ttf',
+  '../CormorantGaramond.css',
+  '../CormorantGaramond.ttf',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+urlsToCache.push("Demosthenes_Against_Pantaenetus_37.html");
+urlsToCache.push("translation.js");
